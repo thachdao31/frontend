242e63c407d4a1a01bce45e7fb0fdd49075c806d
thachdao31 <thachdao31@gmail.com>
2023-01-10 17:09:20 +0700

    van la update

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.css']
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit {
+  @Input() viewMode = false;
 
+  @Input() currentUser: User = {
+    name: '',
+    age: 0,
+    class: ''
+  };
+
+  message = '';
+
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    if(!this.viewMode) {
+      this.message = '';
+      this.getUser(this.route.snapshot.params["id"]);
+    }
+  }
+
+  getUser(id: string): void {
+    this.userService.get(id)
+        .subscribe({
+          next: (data) => {
+            this.currentUser = data;
+            console.log(data);
+          },
+          error: (err) => console.log(err)
+        });
+  }
+
+  updateUser(): void {
+    
+  }
+
+  deleteUser(): void {
+    this.userService.delete(this.currentUser._id)
+        .subscribe({
+          next: (res) => {
+            console.log(res);
+            this.router.navigate(['/']);
+          },
+          error: (err) => console.log(err)
+        });
+  }
 }
--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -9,13 +9,45 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class UserListComponent implements OnInit {
   users?: User[];
+  currentUser : User = {};
+  currentIndex = -1;
+
   constructor(private userServices: UserService) {}
 
   ngOnInit(): void {
-      this.userServices.getAll()
-          .subscribe( data => {
+    this.retrieveUsers();
+  }
+
+  retrieveUsers(): void {
+    this.userServices.getAll()
+        .subscribe({
+          next:(data) => {
             this.users = data;
             console.log(data);
-          });
+          },
+          error: (err) => console.log(err)
+        });
+  }
+
+  refreshList(): void {
+    this.retrieveUsers();
+    this.currentUser = {};
+    this.currentIndex = -1;
+  }
+
+  setActiveUser(user: User, index: number): void {
+    this.currentUser = user,
+    this.currentIndex = index
+  }
+
+  removeAllUsers(): void {
+    this.userServices.deleteAll()
+        .subscribe({
+          next: (res) => {
+            console.log(res);
+            this.refreshList();
+          },
+          error: (err) => console.log(err)
+        })
   }
 }
